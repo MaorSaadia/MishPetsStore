10af2749e5164a5596e88df6f116a3d3cabc05f1
Maor <maorsa9@gmail.com>
2024-11-06 20:33:42 +0200

    Responsive navbar (mobile menu)

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -11,6 +12,7 @@ import logo from "@/assets/logo.png";
 
 import ShoppingCartButton from "./ShoppingCartButton";
 import MainNavigation from "./MainNavigation";
+import MobileMenu from "./MobileMenu";
 
 export default async function Navbar() {
   const wixClient = getWixServerClient();
@@ -24,16 +26,28 @@ export default async function Navbar() {
   return (
     <header className="bg-background shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-5 p-5">
+        <Suspense>
+          <MobileMenu
+            collections={collections}
+            loggedInMember={loggedInMember}
+          />
+        </Suspense>
         <div className="flex flex-wrap items-center gap-5">
           <Link href="/" className="flex items-center gap-4">
-            <Image src={logo} alt="Mish Pets logo" width={40} height={40} />
-            <span className="text-xl font-bold">Mish Pets</span>
+            <Image src={logo} alt="Flow Shop logo" width={40} height={40} />
+            <span className="text-xl font-bold">Flow Shop</span>
           </Link>
-          <MainNavigation collections={collections} />
+          <MainNavigation
+            collections={collections}
+            className="hidden lg:flex"
+          />
         </div>
-        <SearchField className="max-w-96" />
+        <SearchField className="hidden max-w-96 lg:inline" />
         <div className="flex items-center justify-center gap-5">
-          <UserButton loggedInMember={loggedInMember} />
+          <UserButton
+            loggedInMember={loggedInMember}
+            className="hidden lg:inline-flex"
+          />
           <ShoppingCartButton initialData={cart} />
         </div>
       </div>
