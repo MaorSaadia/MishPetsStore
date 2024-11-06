aa7261a64228f26ac69a347abfa1c2543a0b5ef9
Maor <maorsa9@gmail.com>
2024-11-06 12:04:29 +0200

    working on Sort & filter products, URL state, useOptimistic

--- src/wix-api/products.ts
+++ src/wix-api/products.ts
@@ -1,6 +1,4 @@
 import { cache } from "react";
-
-import { WIX_STORES_APP_ID } from "@/lib/constants";
 import { WixClient } from "@/lib/wix-client.base";
 
 export type ProductsSort = "last_updated" | "price_asc" | "price_desc";
@@ -71,6 +69,8 @@ export async function queryProducts(
 
 export const getProductBySlug = cache(
   async (wixClient: WixClient, slug: string) => {
+    console.log("getProductBySlug");
+
     const { items } = await wixClient.products
       .queryProducts()
       .eq("slug", slug)
@@ -86,49 +86,3 @@ export const getProductBySlug = cache(
     return product;
   },
 );
-
-export async function getProductById(wixClient: WixClient, productId: string) {
-  const result = await wixClient.products.getProduct(productId);
-  return result.product;
-}
-
-export async function getRelatedProducts(
-  wixClient: WixClient,
-  productId: string,
-) {
-  const result = await wixClient.recommendations.getRecommendation(
-    [
-      {
-        _id: "68ebce04-b96a-4c52-9329-08fc9d8c1253", // "From the same categories"
-        appId: WIX_STORES_APP_ID,
-      },
-      {
-        _id: "d5aac1e1-2e53-4d11-85f7-7172710b4783", // "Frequenly bought together"
-        appId: WIX_STORES_APP_ID,
-      },
-    ],
-    {
-      items: [
-        {
-          appId: WIX_STORES_APP_ID,
-          catalogItemId: productId,
-        },
-      ],
-      minimumRecommendedItems: 3,
-    },
-  );
-
-  const productIds = result.recommendation?.items
-    .map((item) => item.catalogItemId)
-    .filter((id) => id !== undefined);
-
-  if (!productIds || !productIds.length) return [];
-
-  const productsResult = await wixClient.products
-    .queryProducts()
-    .in("_id", productIds)
-    .limit(4)
-    .find();
-
-  return productsResult.items;
-}
