b258425d3ed5dc8f82f4c4224fee508cb1da1200
Maor <maorsa9@gmail.com>
2024-11-08 13:24:44 +0200

    Update member info (React Hook form)

---
+++ src/hooks/members.ts
@@ -0,0 +1,32 @@
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+
+import { updateMemberInfo, UpdateMemberInfoValues } from "@/wix-api/members";
+import { wixBrowserClient } from "@/lib/wix-client.browser";
+import { useToast } from "./use-toast";
+
+export function useUpdateMember() {
+  const { toast } = useToast();
+
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: (variables: UpdateMemberInfoValues) =>
+      updateMemberInfo(wixBrowserClient, variables),
+    onSuccess() {
+      toast({
+        description: "Profile updated",
+      });
+      setTimeout(() => {
+        router.refresh();
+      }, 2000);
+    },
+    onError(error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to update profile. Please try again.",
+      });
+    },
+  });
+}
